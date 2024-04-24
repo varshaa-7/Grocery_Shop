5892ee1774be51777bfac33e35fca571cc504b05
Varsha <awasthivarsha0710@gmail.com>
2024-04-25 00:12:31 +0530

    completed header
    completed header

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -21,9 +21,21 @@ function Header() {
         height={80}
         />
         
-            <h2 className='hidden md:flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200'>
+            
+            <DropdownMenu>
+            <DropdownMenuTrigger asChild><h2 className='hidden md:flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200'>
                 <LayoutGrid className='h-5 w-5'/>Category
-            </h2>
+            </h2></DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+            </DropdownMenuContent>
+            </DropdownMenu>
+
 
       <div className='md:flex gap-3 items-center border rounded-full p-2 px-5 hidden'>
         <Search/>
